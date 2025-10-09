f88a8d6540855de764c007e0970d36c857b50210
taufiqGit <wecannn29@gmail.com>
2025-10-09 09:08:56 +0700

    -

---
+++ reflect.go
@@ -0,0 +1,35 @@
+package main
+import "fmt"
+import "reflect"
+
+
+type student struct {
+	Name  string
+	Grade int
+}
+
+func (s *student) getPropertyInfo() {
+	var reflectValue = reflect.ValueOf(s)
+	fmt.Println("Type :", reflectValue.Type(), reflectValue)
+
+	if reflectValue.Kind() == reflect.Ptr {
+		reflectValue = reflectValue.Elem()
+	}
+
+	var reflectType = reflectValue.Type()
+	
+	for i := 0; i < reflectValue.NumField(); i++ {
+        fmt.Println("nama      :", reflectType.Field(i).Name)
+        fmt.Println("tipe data :", reflectType.Field(i).Type)
+        fmt.Println("nilai     :", reflectValue.Field(i).Interface())
+        fmt.Println("")
+    }
+}
+
+func main() {
+	var s1 = &student{
+		Name:  "Taufiqs",
+		Grade: 90,
+	}
+	s1.getPropertyInfo()
+}
